76aa3fe6eab7eb531e85c440f7fd2651381248d3
mirahs <mirahs@163.com>
2017-08-27 20:51:59 +0800

    修改

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,7 +6,7 @@ public class Test : MonoBehaviour
 {
     void Start()
     {
-        GameObject go = ResMgr.Inst.CreateGO("A/Cube");
+        GameObject go = ResMgr.Inst.CreateGO("B/Capsule");
         go.transform.position = new Vector3(0, 0, 0);
     }
 }
