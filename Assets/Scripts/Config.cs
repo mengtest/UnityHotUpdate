235f351d79437b5e67196bef9ea0b261e426abc9
mirahs <mirahs@163.com>
2017-08-27 15:49:54 +0800

    修改

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public class Config
+{
+    public const int ver_android_res = 1;
+    public const int ver_ios_res = 1;
+    public const int ver_android_lua = 1;
+    public const int ver_ios_lua = 1;
+}
