d5c79fe0842fe06447292a7658a0154ac82af816
mirahs <mirahs@163.com>
2017-08-27 09:43:58 +0800

    修改

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEditor;
+
+
+public class BuildAssetBundle
+{
+    [MenuItem("Game/Build AssetBundles(Android)")]
+    private static void BuildAssetBundlesAndroid()
+    {
+        SetAssetBundleName();
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/../AssetBundle/", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.Android);
+    }
+
+    [MenuItem("Game/Build AssetBundles(iOS)")]
+    private static void BuildAssetBundlesIos()
+    {
+        SetAssetBundleName();
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/../AssetBundle", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.iOS);
+    }
+
+
+    private static void SetAssetBundleName()
+    {
+        string fullPath = Application.dataPath + "/Resources/";
+        var relativeLen = "Assets/Resources/".Length; // Assets 长度
+        if (Directory.Exists(fullPath))
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(fullPath);
+            FileInfo[] files = dirInfo.GetFiles("*", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; i++)
+            {
+                var fileInfo = files[i];
+                if (files[i].Name.EndsWith(".meta")) { continue; }
+
+                var basePath = fileInfo.FullName.Substring(fullPath.Length - relativeLen).Replace('\\', '/');
+                //Debug.Log("basePath: " + basePath);
+
+                string extName = fileInfo.Extension;
+                string assetBundleName = Regex.Split(basePath.Substring(relativeLen), extName, RegexOptions.IgnoreCase)[0];
+                //Debug.Log("assetBundleName: " + assetBundleName);
+
+                AssetImporter importer = AssetImporter.GetAtPath(basePath);
+                importer.assetBundleName = assetBundleName;
+            }
+        }
+        else
+        {
+            Debug.LogError(fullPath + " not exists!");
+        }
+    }
+}
---
+++ Assets/Scripts/NonCacheBundle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NonCacheBundle : MonoBehaviour
+{
+    private string BundleURL;
+    private string AssetName = "Cube";
+
+
+    void Awake()
+    {
+#if UNITY_ANDROID
+        BundleURL = "jar:file://" + Application.persistentDataPath + "/../AssetBundle/A/Cube";
+#elif UNITY_IPHONE
+        BundleURL = Application.persistentDataPath + "/../AssetBundle/A/Cube";
+#elif UNITY_STANDALONE_WIN || UNITY_STANDALONE_MAC || UNITY_EDITOR
+        BundleURL = "file://" + Application.dataPath + "/../AssetBundle/A/Cube";
+#else
+        BundleURL = string.Empty;
+#endif
+        Debug.Log("Application.persistentDataPath: " + Application.persistentDataPath);
+    }
+
+
+    IEnumerator Start()
+    {
+        WWW www = new WWW(BundleURL);
+        yield return www;
+        if (www.error != null)
+        {
+            throw new Exception("WWW download had an error: " + www.error);
+        }
+        AssetBundle bundle = www.assetBundle;
+        GameObject cube = Instantiate(bundle.LoadAsset(AssetName)) as GameObject;
+        cube.transform.position = new Vector3(0, 0, 0);
+        bundle.Unload(false);
+        www.Dispose();
+    }
+}
