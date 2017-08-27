7acb64c27693bbbee7b1ba35bbb62aaf971055e0
mirahs <mirahs@163.com>
2017-08-27 19:39:20 +0800

    修改

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,19 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
+
+public enum Platform
+{ 
+    None,
+    Android,
+    iOS,
+}
 
 public class Config
 {
+    public static string ApiUrl = "http://192.168.0.104:81/";
+
+    public static int VerInit = 1;
     public static int VerAndroidRes = 1;
     public static int VerIosRes = 1;
     public static int VerAndroidLua = 1;
     public static int VerIosLua = 1;
 
+    public static string ResInfoFile = Application.dataPath + "/Resources/resourcesinfo";
 #if UNITY_ANDROID
+    public static Platform platform = Platform.Android;
     public static string AssetBundlePath = "jar:file://" + Application.persistentDataPath + "/../AssetBundle/";
     public static string AssetBundleFile = AssetBundlePath + "AssetBundle";
+    public static string HotResInfoFile = AssetBundlePath + "resourcesinfo";
 #elif UNITY_IOS
+    public static Platform platform = Platform.iOS;
     public static string AssetBundlePath = Application.persistentDataPath + "/../AssetBundle/";
     public static string AssetBundleFile = AssetBundlePath + "AssetBundle";
+    public static string HotResInfoFile = AssetBundlePath + "resourcesinfo";
+#else
+    public static Platform platform = Platform.None;
+    public static string AssetBundlePath = "";
+    public static string AssetBundleFile = "";
+    public static string HotResInfoFile = "";
 #endif
+
+    public static string ApiVersion = ApiUrl + platform + "/version";
 }
--- Assets/Scripts/HotUpdate.cs
+++ Assets/Scripts/HotUpdate.cs
@@ -1,7 +1,18 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+
+public class VersionInfo
+{
+    public int ver_android_res;
+    public int ver_ios_res;
+    public int ver_android_lua;
+    public int ver_ios_lua;
+}
 
 
 public class HotUpdate : MonoBehaviour
@@ -14,13 +25,179 @@ public class HotUpdate : MonoBehaviour
 #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_MAC || UNITY_EDITOR
         SceneManager.LoadScene("Main");
 #else
-        StartCoroutine(CheckUpdate());
+        StartCoroutine(DoUpdate());
 #endif
     }
 
 
-    IEnumerator CheckUpdate()
+
+    private void GetResInfoFile()
     {
-        yield return null;
+        if (File.Exists(Config.HotResInfoFile))
+        {
+            resInfoFile = Config.HotResInfoFile;
+        }
+        else
+        {
+            resInfoFile = Config.ResInfoFile;
+        }
+    }
+
+    IEnumerator DoUpdate()
+    {
+        if (!File.Exists(resInfoFile))
+        {
+            Debug.LogError("resourcesinfo file not exist");
+            yield break;
+        }
+
+        WWW www = new WWW(Config.ApiVersion);
+        yield return www;
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.LogError("request version error: " + www.error);
+            yield break;
+        }
+
+        VersionInfo versionInfo = JsonUtility.FromJson<VersionInfo>(www.text);
+
+        int verLocal = GetLocalVersion();
+        int verServer = GetServerVersion(versionInfo);
+
+        StartCoroutine(DoUpdate(verLocal, verServer));
+    }
+
+    IEnumerator DoUpdate(int verLocal, int verServer)
+    {
+        if (verLocal >= verServer)
+        {
+            SceneManager.LoadScene("Main");
+        }
+        else
+        {
+            verLocal++;
+            string verInfoFile = Config.ApiUrl + Config.platform + "/v" + verLocal + "/resourcesinfo";
+            WWW wwwResInfo = new WWW(verInfoFile);
+            yield return wwwResInfo;
+            if (!string.IsNullOrEmpty(wwwResInfo.error))
+            {
+                Debug.LogError("request " + verInfoFile + " error: " + wwwResInfo.error);
+                yield break;
+            }
+
+            DoLocalResInfoSign();
+
+            string[] fileLines = Regex.Split(wwwResInfo.text, "\n", RegexOptions.IgnoreCase);
+            foreach (string fileLine in fileLines)
+            {
+
+                string[] fileAndSign = Regex.Split(fileLine, ":", RegexOptions.IgnoreCase);
+                string file = fileAndSign[0];
+
+                bool needDown = false;
+
+                if (mLocalResInfoSing.ContainsKey(file))
+                {
+                    string oldSign = mLocalResInfoSing[file];
+                    string newSign = fileAndSign[1];
+                    needDown = (oldSign == newSign) ? true : false;
+                }
+                else
+                {
+                    needDown = true;
+                }
+                if (needDown)
+                {
+                    string downFile = Config.ApiUrl + Config.platform + "/v" + verLocal + "/" + file;
+                    WWW wwwDownFile = new WWW(downFile);
+                    yield return wwwDownFile;
+                    if (!string.IsNullOrEmpty(wwwDownFile.error))
+                    {
+                        Debug.LogError("request " + downFile + " error: " + wwwDownFile.error);
+                        yield break;
+                    }
+                    Save2LocalFile(file, wwwDownFile.bytes, wwwDownFile.bytes.Length);
+                }
+            }
+            //后续工作
+            Save2LocalFile("resourcesinfo", wwwResInfo.bytes, wwwResInfo.bytes.Length);
+            SetLocalVersion(verLocal);
+
+            StartCoroutine(DoUpdate(verLocal, verServer));
+        }
+    }
+
+    private Dictionary<string, string> mLocalResInfoSing = new Dictionary<string, string>();
+    private void DoLocalResInfoSign()
+    { 
+        using(FileStream fs = new FileStream(resInfoFile, FileMode.Open, FileAccess.Read))
+        using (StreamReader sr = new StreamReader(fs))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] fileAndSign = Regex.Split(line, ":", RegexOptions.IgnoreCase);
+                mLocalResInfoSing.Add(fileAndSign[0], fileAndSign[1]);
+            }
+        }
+    }
+
+    private int GetServerVersion(VersionInfo versionInfo)
+    {
+        if (Config.platform == Platform.Android)
+        {
+            return versionInfo.ver_android_res;
+        }
+        else
+        {
+            return versionInfo.ver_ios_res;
+        }
+    }
+
+    private int GetLocalVersion()
+    {
+        if (Config.platform == Platform.Android)
+        {
+            return PlayerPrefs.GetInt("ver_android_res", Config.VerInit);
+        }
+        else
+        {
+            return PlayerPrefs.GetInt("ver_ios_res", Config.VerInit);
+        }
+    }
+
+    private void SetLocalVersion(int version)
+    {
+        if (Config.platform == Platform.Android)
+        {
+            PlayerPrefs.SetInt("ver_android_res", version);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("ver_ios_res", version);
+        }
+    }
+
+    private void Save2LocalFile(string file, byte[] data, int dataLen)
+    {
+        string[] filePaths = Regex.Split(file, "/", RegexOptions.IgnoreCase);
+        if (filePaths.Length > 1)
+        {
+            string[] filePathsTmp = new string[filePaths.Length - 1];
+            System.Array.Copy(filePaths, 0, filePathsTmp, 0, filePaths.Length - 1);
+            string filePath = string.Join("/", filePathsTmp);
+            filePath = Config.AssetBundlePath + filePath;
+            if (!Directory.Exists(filePath)) Directory.CreateDirectory(filePath);
+        }
+
+        file = Config.AssetBundlePath + file;
+
+        FileInfo fileInfo = new FileInfo(file);
+        if (fileInfo.Exists) fileInfo.Delete();
+
+        Stream sw = fileInfo.Create();
+        sw.Write(data, 0, dataLen);
+        sw.Flush();
+        sw.Close();
     }
 }
