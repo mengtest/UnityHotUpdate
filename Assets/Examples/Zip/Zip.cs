de407e568081729e14add939dbb337565927baee
mirahs <mirahs@163.com>
2017-09-10 10:43:01 +0800

    添加压缩解压工具

---
+++ Assets/Examples/Zip/Zip.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Zip : MonoBehaviour
+{
+    private string _zipSrcPath = "";
+    private string _zipSrcFile = "";
+    private string _zipDestPath = "";
+
+
+	void Start()
+    {
+        _zipSrcPath = Application.dataPath + "/Examples/Zip/Src/";
+        _zipSrcFile = Application.dataPath + "/Examples/Zip/Src.zip";
+        _zipDestPath = Application.dataPath + "/Examples/Zip/Dest/";
+
+        Dictionary<string, string> dictFile = new Dictionary<string, string>();
+
+        DirectoryInfo dirInfo = new DirectoryInfo(_zipSrcPath);
+        FileInfo[] filesInfo = dirInfo.GetFiles("*", SearchOption.AllDirectories);
+        for (int i = 0; i < filesInfo.Length; i++)
+        {
+            var fileInfo = filesInfo[i];
+            if (fileInfo.Name.EndsWith(".meta")) { continue; }
+            Debug.Log("fileInfo.FullName: " + fileInfo.FullName);
+            string relativeName = fileInfo.FullName.Substring(_zipSrcPath.Length);
+            Debug.Log("relativeName:" + relativeName);
+            dictFile.Add(fileInfo.FullName, relativeName);
+        }
+
+        UtilZip.Zip(dictFile, _zipSrcFile);
+
+        UtilZip.UnZip(_zipSrcFile, _zipDestPath);
+	}
+}
---
+++ Assets/Scripts/UtilZip.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+
+public class UtilZip
+{
+    /// <summary>
+    /// 压缩多个文件
+    /// </summary>
+    /// <param name="inFiles">压缩文件字典(文件名-标识名)</param>
+    /// <param name="outFile">压缩后的文件名</param>
+    public static void Zip(Dictionary<string, string> inFiles, string zipFile)
+    {
+        using (ZipFile zip = ZipFile.Create(zipFile))
+        {
+            zip.BeginUpdate();
+
+            foreach (var kv in inFiles)
+            {
+                zip.Add(kv.Key, kv.Value);
+            }
+
+            zip.CommitUpdate();
+        }
+    }
+
+    /// <summary>
+    /// 压缩多个文件
+    /// </summary>
+    /// <param name="inFiles">压缩文件列表</param>
+    /// <param name="outFile">压缩后文件名</param>
+    public static void Zip(List<string> inFiles, string zipFile)
+    {
+        using (ZipFile zip = ZipFile.Create(zipFile))
+        {
+            zip.BeginUpdate();
+
+            foreach (string inFile in inFiles)
+            {
+                zip.Add(inFile);
+            }
+
+            zip.CommitUpdate();
+        }
+    }
+
+    /// <summary>
+    /// 解压文件
+    /// </summary>
+    /// <param name="zipFile">压缩文件</param>
+    /// <param name="outPath">解压目录</param>
+    public static void UnZip(string zipFile, string outPath)
+    {
+        (new FastZip()).ExtractZip(zipFile, outPath, "");
+    }
+}
